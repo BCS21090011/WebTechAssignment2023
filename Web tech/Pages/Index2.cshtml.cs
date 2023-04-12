e3d10921c08bdc3139e8c9f5eee42c3d70f71779
howardyam <howwei92@gmail.com>
2023-04-12 23:34:14 +0800

    Add question first version

---
+++ Web tech/Pages/Index2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_tech.Pages
+{
+    public class Index2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
