359425ff74104079c9b914380dc9bc6fd5ef5261
howardyam <howwei92@gmail.com>
2023-04-13 09:53:13 +0800

    Dashboard interface upadate

---
+++ Web tech/Pages/Index4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_tech.Pages
+{
+    public class Index4Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
