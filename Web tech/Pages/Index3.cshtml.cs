b1cbfbc8aeb3687288cbce795171204832bb5bcb
howardyam <howwei92@gmail.com>
2023-04-13 09:09:54 +0800

    Dashboard update

---
+++ Web tech/Pages/Index3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_tech.Pages
+{
+    public class Index3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
