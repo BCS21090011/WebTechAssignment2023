ae08151c747584aa7eea36d3dab54608af867e10
howardyam <howwei92@gmail.com>
2023-04-28 15:39:26 +0800

    profilepage

---
+++ Web tech/Pages/ProfilePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_tech.Pages
+{
+    public class ProfilePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
