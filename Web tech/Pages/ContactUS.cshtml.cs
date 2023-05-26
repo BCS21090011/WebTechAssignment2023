a7cbbaccaa3cfb869a4fcd089b7c7122e9628d3d
howardyam <howwei92@gmail.com>
2023-05-26 21:41:33 +0800

    some changes interface

--- Web tech/Pages/ContactUS.cshtml.cs
+++ Web tech/Pages/ContactUS.cshtml.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace Web_tech.Pages
 {
+    [Authorize]
     public class ContactUSModel : PageModel
     {
         public void OnGet()
