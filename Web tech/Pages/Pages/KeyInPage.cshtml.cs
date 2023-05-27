5679936e62767343ab6a68c4c74090094f78adf8
song1351 <songkimhian@hotmail.com>
2023-05-27 20:15:24 +0800

    KEY in Page Done

--- Web tech/Pages/Pages/KeyInPage.cshtml.cs
+++ Web tech/Pages/Pages/KeyInPage.cshtml.cs
@@ -1,4 +1,5 @@
 // KeyInPage.cshtml.cs
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,44 +15,41 @@ namespace Web_tech.Pages
     [Authorize(Roles = "Admin")]
     public class KeyInPageModel : PageModel
     {
+
         private readonly DatabaseContext _context;
 
         public KeyInPageModel(DatabaseContext context)
         {
             _context = context;
         }
+        [BindProperty]
+        public Questions? Questions { get; set; }
 
         public void OnGet()
         {
-        }
 
-        public async Task<IActionResult> OnPostAsync()
+        }
+        public async Task<IActionResult> OnPost()
         {
-            // Get the data from the form
-            string difficulty = Request.Form["difficulty"];
-            string subject = Request.Form["subject"];
-            string question = Request.Form["question"];
-            string answer = Request.Form["answer"];
-            int marks = Convert.ToInt32(Request.Form["marks"]);
-
-            // Create a new Questions object
-            var newQuestion = new Questions
+            if (ModelState.IsValid)
             {
-                Question = question,
-                QuestionMark = marks,
-                QuestionDifficulty = Convert.ToInt32(difficulty),
-                SubjectsId = Convert.ToInt32(subject),
-                QuestionImageFileName = "", // Set the value for the image file name if applicable
-                Answer = answer,
-                AnswerImageFileName = "" // Set the value for the answer image file name if applicable
-            };
 
-            // Add the new question to the database
-            _context.Questions.Add(newQuestion);
-            await _context.SaveChangesAsync();
 
-            // Redirect to the desired page
-            return RedirectToPage("/Index");
+                // Save the data to the database
+                await _context.Questions.AddAsync(Questions);
+                await _context.SaveChangesAsync();
+                Console.WriteLine("Success");
+
+
+                return RedirectToPage("/Pages/Keyinpage");
+            }
+            else
+            {
+                return Page();
+            }
         }
+
     }
 }
+
+
