dcfdaa3fcfeb2de858a661049b1600f85ac1fcd2
howardyam <howwei92@gmail.com>
2023-05-31 14:53:36 +0800

    Added filter in QuestionBank page. Need to solve the encoded text problem when create new questions.

--- Web tech/wwwroot/js/CreateQuestionsVer5.js
+++ Web tech/wwwroot/js/CreateQuestionsVer5.js
@@ -268,23 +268,4 @@ function UpdateSelectedQstsLst() {
     console.log("Done update selectedQsts list");
 }
 
-function FilterQsts(target, object) {
-    // Create a Set of IDs from the object array for faster lookup
-    const objectIds = new Set(object.map(item => item.ID));
-
-    // Filter the target array based on the IDs
-    const filteredTarget = target.filter(item => !objectIds.has(item.ID));
-
-    // Return the filtered target array
-    return filteredTarget;
-}
-
-function FilterQstsByAttr(target, attr, value) {
-    // Filter the target array based on the attribute and value
-    const filteredTarget = target.filter(item => item[attr] === value);
-
-    // Return the filtered target array
-    return filteredTarget;
-}
-
 // Initializing();
